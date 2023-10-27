f3b7889808ecc79e5a5708be41aa68ac7b7a08e6
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-10-27 12:03:29 +0700

    Update code

--- hocjs/fetch_api/js/ex01.js
+++ hocjs/fetch_api/js/ex01.js
@@ -40,19 +40,23 @@ POST /users => Thêm mới user
 const app = {
   root: document.querySelector(".posts"),
   sort: document.querySelector(".sorting"),
+  filter: document.querySelector(".filter"),
+  paginate: document.querySelector(".paginate"),
+  modal: document.querySelector("#modal-detail"),
   query: {},
   render: function (posts) {
     const { _order = "desc" } = this.query;
-    this.root.innerHTML = `<div class="row">
+    this.root.innerHTML = `<div class="row g-3">
     ${posts
       .map(
-        ({ title, excerpt, image }) => `<div class="col-6 col-lg-4">
+        ({ id, title, excerpt, image }) => `<div class="col-6 col-lg-4">
     <div class="post-item">
       <img src="${image}" alt="">
       <h3>${title}</h3>
       <p>
         ${excerpt}
       </p>
+      <button class="btn btn-primary btn-sm js-click" data-bs-toggle="modal" data-bs-target="#modal-detail" data-id="${id}">Chi tiết</button>
     </div>
   </div>`,
       )
@@ -67,6 +71,35 @@ const app = {
     _order === "asc" ? "active" : ""
   }" data-sort="asc">Cũ nhất</button>
 </div>`;
+
+    this.filter.innerHTML = `<form class="mb-3 form">
+    <input type="search" class="form-control" placeholder="Từ khóa tìm kiếm..." value="${
+      this.query.q ?? ""
+    }"/>
+  </form>`;
+  },
+  renderPaginate: function (totalPage) {
+    this.paginate.innerHTML = `<ul class="pagination justify-content-end pagination-sm">
+    ${
+      this.query._page > 1
+        ? `<li class="page-item"><a class="page-link page-prev" href="#">Trước</a></li>`
+        : ""
+    }
+    ${[...Array(totalPage).keys()]
+      .map(
+        (index) =>
+          `<li class="page-item ${
+            this.query._page === +index + 1 ? "active" : ""
+          }"><a class="page-link page-number" href="#">${index + 1}</a></li>`,
+      )
+      .join("")}
+  
+      ${
+        this.query._page < totalPage
+          ? `<li class="page-item"><a class="page-link page-next" href="#">Sau</a></li>`
+          : ""
+      }
+  </ul>`;
   },
   eventSort: function () {
     this.sort.addEventListener("click", (e) => {
@@ -77,19 +110,92 @@ const app = {
       }
     });
   },
+  eventFilter: function () {
+    this.filter.addEventListener("submit", (e) => {
+      if (e.target.classList.contains("form")) {
+        e.preventDefault(); //Ngăn load lại trang khi submit
+
+        const keyword = e.target.querySelector("input").value;
+        this.query.q = keyword;
+        this.fetch();
+      }
+    });
+  },
+  eventPaginate: function () {
+    this.paginate.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target.classList.contains("page-number")) {
+        const page = e.target.innerText;
+        this.query._page = +page;
+      }
+
+      if (e.target.classList.contains("page-next")) {
+        this.query._page++;
+      }
+
+      if (e.target.classList.contains("page-prev")) {
+        this.query._page--;
+      }
+
+      this.fetch();
+    });
+  },
+  eventDetail: function () {
+    this.root.addEventListener("click", (e) => {
+      if (e.target.classList.contains("js-click")) {
+        e.preventDefault();
+        const postId = e.target.dataset.id;
+        this.fetchDetail(postId);
+      }
+    });
+  },
+  eventModal: function () {
+    this.modal.addEventListener("hidden.bs.modal", () => {
+      const modalTitle = this.modal.querySelector(".modal-title");
+      const modalContent = this.modal.querySelector(".modal-body");
+      modalTitle.innerText = "";
+      modalContent.innerText = "";
+    });
+  },
   fetch: async function () {
     const queryString = new URLSearchParams(this.query).toString();
     const response = await fetch(`http://localhost:3000/posts?${queryString}`);
     const posts = await response.json();
     this.render(posts);
+
+    //Tính tổng số trang
+    const totalPosts = response.headers.get("x-total-count");
+    const totalPage = Math.ceil(totalPosts / 3);
+    this.renderPaginate(totalPage);
+
+    window.scroll({
+      top: 0,
+    });
+  },
+  fetchDetail: async function (postId) {
+    const response = await fetch(`http://localhost:3000/posts/${postId}`);
+
+    if (response.ok) {
+      const { title, content } = await response.json();
+      const modalTitle = this.modal.querySelector(".modal-title");
+      const modalContent = this.modal.querySelector(".modal-body");
+      modalTitle.innerText = title;
+      modalContent.innerHTML = content;
+    }
   },
   start: function () {
     Object.assign(this.query, {
       _order: "desc",
       _sort: "id",
+      _limit: 3,
+      _page: 1,
     });
     this.fetch();
     this.eventSort();
+    this.eventFilter();
+    this.eventPaginate();
+    this.eventDetail();
+    this.eventModal();
   },
 };
 
